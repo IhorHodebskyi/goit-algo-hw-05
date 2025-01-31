db7945a3f12e7cdd447fbafc3f38ced0643340df
ihor <i0962112822@gmail.com>
2025-01-31 20:58:01 +0200

    fix

--- main.py
+++ main.py
@@ -1,41 +1,36 @@
 
 import re
+from typing import Callable, Generator
 
 
-def caching_fibonacci( ):
-    cache = {}
+def caching_fibonacci( )->Callable[[int], int]:
+    cache: dict[int, int] = {}
 
-    def fibonacci(n):
-        if n <= 0:
+    def fibonacci(value: int)-> int:
+        if value <= 0:
             return 0
-        if n == 1:
+        if value == 1:
             return 1
-        if n in cache:
-            return cache[n]
-        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
-        return cache[n]
+        if value in cache:
+            return cache[value]
+        cache[value] = fibonacci(value - 1) + fibonacci(value - 2)
+        return cache[value]
 
     return fibonacci
 
 
 
-def generator_numbers(values):
-
-    suma = 0
-    for value in values:
-        yield value
-        suma += float(value)
-
-    return suma
+def generator_numbers(value: str)->Generator[float, None, None]:
+    list_number = re.findall(r"\d+\.\d+", value)
+    for number in list_number:
+        yield float(number)
 
 
-def sum_profit(text):
+
+def sum_profit(value: str, func: Callable[[str],Generator[float,None, None]]):
+    return sum(func(value))
     
-    list_number = re.findall(r"\d+\.\d+", text)
-    sum = 0
-    for value in list_number:
-        sum += float(value)
-    return sum
+
 
 
 
@@ -49,5 +44,5 @@ if __name__ == '__main__':
 
 
     text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
-    total_income = sum_profit(text)
+    total_income = sum_profit(text, generator_numbers)
     print(f"Загальний дохід: {total_income}")
