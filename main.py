35b22028c76a6f1725072737824b35a476ebf8ab
ihor <i0962112822@gmail.com>
2025-01-31 10:02:56 +0200

    fix

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import re
 
 
-def caching_fibonacci(n, cache = {}):
+def caching_fibonacci(n = 1, cache = {}):
 
 
     def fibonacci(n):
@@ -15,7 +15,7 @@ def caching_fibonacci(n, cache = {}):
         cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
         return cache[n]
 
-    return fibonacci(n)
+    return fibonacci
 
 
 
@@ -29,11 +29,11 @@ def generator_numbers(values):
     return suma
 
 
-def sum_profit(text, generator_function):
+def sum_profit(text):
     
-    generator = generator_function(re.findall(r"\d+\.\d+", text))
+    list_number = re.findall(r"\d+\.\d+", text)
     sum = 0
-    for value in generator:
+    for value in list_number:
         sum += float(value)
     return sum
 
@@ -41,11 +41,13 @@ def sum_profit(text, generator_function):
 
 if __name__ == '__main__':
     n = 15
-    print(caching_fibonacci(n))
+    fib = caching_fibonacci()
+    print(fib(10))  # Виведе 55
+    print(fib(15))  # Виведе 610
 
 
 
 
     text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
-    total_income = sum_profit(text, generator_numbers)
+    total_income = sum_profit(text)
     print(f"Загальний дохід: {total_income}")
